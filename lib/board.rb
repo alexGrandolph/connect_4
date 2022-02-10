6fe39b47dc8423ff57c42840441ea65fa3f9b74b
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-10 13:21:38 -0700

    Add class test and code to pass it

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,26 @@
 # Board class
+class Board
+  attr_reader :board, :fresh_board
+  def initialize
+    @board = board
+    @fresh_board = { A1: ".", B1: ".", C1: ".", D1: ".", E1: ".", F1: ".", G1: ".",
+                    A2: ".", B2: ".", C2: ".", D2: ".", E2: ".", F2: ".", G2: ".",
+                    A3: ".", B3: ".", C3: ".", D3: ".", E3: ".", F3: ".", G3: ".",
+                    A4: ".", B4: ".", C4: ".", D4: ".", E4: ".", F4: ".", G4: ".",
+                    A5: ".", B5: ".", C5: ".", D5: ".", E5: ".", F5: ".", G5: ".",
+                    A6: ".", B6: ".", C6: ".", D6: ".", E6: ".", F6: ".", G6: ".",}
+
+  end
+  def print_board
+        rows = ["1", "2", "3", "4", "5", "6"].reverse
+        rows.each do |row|
+          current_row = {}
+          @fresh_board.each do |key, val|
+            if key.to_s.include?row
+              current_row[key] = val
+            end
+          end
+          puts current_row.values.join
+        end
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,13 @@
 # Tests for Board class
+require 'rspec'
+require './lib/board'
+
+RSpec.describe Board do
+  it "class exists" do
+    new_board = Board.new
+    expect(new_board).to be_an_instance_of(Board)
+  end
+
+
+
+end
